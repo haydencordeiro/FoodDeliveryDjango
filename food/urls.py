985c7d596ef9f765f9e1d5d6231e2fdc12c51d35
Hayden Corderio <cordeirohayden@gmail.com>
2021-03-27 21:54:06 +0530

    singleshopallproducts added in urls

--- food/urls.py
+++ food/urls.py
@@ -102,5 +102,11 @@ urlpatterns = [
 
 
 
+    path('api/singleshopallproducts/', SingleShopAllProducts,
+         name='SingleShopAllProducts'),
+
+
+
+
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
